0118c5a7972472913fea0159066dc76ac0001cf2
manideepreddy <manideepreddytippana@gmail.com>
2026-06-16 18:41:10 +0530

    small config change

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
@@ -11,9 +11,9 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <AuthContextProvider>
-        <HashRouter>
+        <BrowserRouter>
           <App />
-        </HashRouter>
+        </BrowserRouter>
       </AuthContextProvider>
     </Provider>
   </StrictMode>,
