62b5a19d41e771eebde8c8635097d2fe516e807a
manideepreddy <manideepreddytippana@gmail.com>
2026-06-16 18:38:04 +0530

    updated with hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
@@ -11,9 +11,9 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <AuthContextProvider>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </AuthContextProvider>
     </Provider>
   </StrictMode>,
